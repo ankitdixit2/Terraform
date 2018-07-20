5785b84119e56aa7002798dad6e6aef18bc43b74
ankitdixit2 <39045902+ankitdixit2@users.noreply.github.com>
2018-07-20 08:42:36 +0530

    Update eia-terraform-createvm.tf

--- eia-terraform-createvm.tf
+++ eia-terraform-createvm.tf
@@ -3,21 +3,21 @@ access_key = "AKIAIQKFPYRZQPG7M7MA"
 secret_key = "iSzrPgopEp0fQfcuBwfdAouRpDW1bId+TivbSc5G"
 region = "ap-southeast-1"
 }
-resource "aws_instance" "example3" {
-  ami = "ami-03ecd8a9a905ec288"
-  instance_type = "t2.micro"
-  key_name = "terraformnewkey"
-  security_groups = ["eia_group"]
-tags {
-  Name = "eiaswarmmaster1"
-}
-}
 resource "aws_instance" "example4" {
   ami = "ami-03ecd8a9a905ec288"
   instance_type = "t2.micro"
   key_name = "terraformnewkey"
   security_groups = ["eia_group"]
 tags {
-  Name = "eiaswarmworker1"
+  Name = "eiaswarmmaster4"
+}
+}
+resource "aws_instance" "example5" {
+  ami = "ami-03ecd8a9a905ec288"
+  instance_type = "t2.micro"
+  key_name = "terraformnewkey"
+  security_groups = ["eia_group"]
+tags {
+  Name = "eiaswarmworker4"
 }
 }
